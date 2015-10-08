53162ed57e0a883353adbc64060c4681766df6c2
Nic Nash <nicnash08@Nic-Nashs-iMac.local>
2015-10-07 20:05:16 -0500

    add ideas to backend

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :ideas
+
   devise_for :users, controllers: { sessions: 'sessions' }
 
   # The priority is based upon order of creation: first created -> highest priority.
